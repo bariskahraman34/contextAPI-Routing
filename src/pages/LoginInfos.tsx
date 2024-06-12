767c7a94a103b162fc7dd3b0e3869cf3f4d783af
Barış Kahraman <baris-kahra@hotmail.com>
2024-06-13 01:12:59 +0300

    Kullanıcı oluşturulduktan sonra kullanıcı tekrar kullanıcı oluştur sayfasına dönmek isterse , oluşturulan kullanıcının bilgilerinin ilgili inputlara verilmesi sağlandı.

--- src/pages/LoginInfos.tsx
+++ src/pages/LoginInfos.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { UserContext } from '../UserProvider';
 import Summary from '../components/Summary';
 import { FormControl,TextField,Button,Stack } from '@mui/material';
@@ -11,7 +11,7 @@ export default function LoginInfos() {
     password: ''
   });
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const { setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
   const handleChange = (e: { target: { name: string; value: string; }; }) => {
       const { name, value } = e.target;
@@ -20,6 +20,17 @@ export default function LoginInfos() {
           [name]: value
       });
   };
+
+  useEffect(() => {
+    if (user) {
+      setFormData({
+        name: user.name || '',
+        email: user.email || '',
+        phone: user.phone || '',
+        password: user.password || ''
+      });
+    }
+  }, [user]);
 
   const handleSubmit = (e: { preventDefault: () => void; }) => {
       e.preventDefault();
@@ -32,10 +43,10 @@ export default function LoginInfos() {
       <Stack spacing={2}> 
         <form onSubmit={handleSubmit}>
           <FormControl fullWidth style={{marginTop:"20px", gap:"20px"}} >
-            <TextField id='fullName' name='name' type='text' label="Ad Soyad" variant='outlined' onChange={handleChange} required />
-            <TextField id='email' name='email' type='email' label="E-Posta" variant='outlined' onChange={handleChange} required />
-            <TextField id='phone' name='phone' type='text' label="Telefon Numarası" variant='outlined' onChange={handleChange} required />
-            <TextField id='password' name='password' type='password' label="Şifre" variant='outlined' onChange={handleChange} required />
+            <TextField value={formData.name} id='fullName' name='name' type='text' label="Ad Soyad" variant='outlined' onChange={handleChange} required />
+            <TextField value={formData.email} id='email' name='email' type='email' label="E-Posta" variant='outlined' onChange={handleChange} required />
+            <TextField value={formData.phone} id='phone' name='phone' type='text' label="Telefon Numarası" variant='outlined' onChange={handleChange} required />
+            <TextField value={formData.password} id='password' name='password' type='password' label="Şifre" variant='outlined' onChange={handleChange} required />
             <Button variant="contained" type='submit'>
               Gönder
             </Button>
